4a2f49336c1e229273e67ad55ddf7de12f07b8eb
Priyadarshini Iyer <priyer@Priyadarshinis-MacBook-Air.local>
2013-12-01 14:22:18 -0800

    Combined Sonali and Priya's inserts

--- stackOverFlow/create_lemma.py
+++ stackOverFlow/create_lemma.py
@@ -3,14 +3,14 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from nltk.stem import WordNetLemmatizer as WNL
-from models_p import LemmaTemp, Base
+from models import LemmaTemp, Base
 
 
 def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import models
+    import models_p
     Base.metadata.create_all(bind=engine)
     print Base.metadata.tables.keys()
     print Base.metadata.reflect(engine)
--- stackOverFlow/insert_tables.py
+++ stackOverFlow/insert_tables.py
@@ -4,18 +4,18 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
+from nltk.stem import WordNetLemmatizer as WNL
+from models import LemmaTemp,Base
 
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import models
+    Base.metadata.create_all(bind=engine)
+    print Base.metadata.tables.keys()
+    print Base.metadata.reflect(engine)
 
-SQLALCHEMY_DATBASE_URI='sqlite:///tutorial.db'
-engine = create_engine(SQLALCHEMY_DATBASE_URI, convert_unicode=True)
-metadata = MetaData(bind=engine)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
-
-Base.metadata.create_all(bind=engine)
 
 def newPhraseInfo(phrase):
     return {"count":0,
@@ -23,13 +23,25 @@ def newPhraseInfo(phrase):
         "phrase":phrase
         }
 
+def insert_db():
+    entries=[('dogs',1),('cats',2),('jumping',3),('happened',4),('best',5),('worst',6)]
+    wnl=WNL()
+    for e in entries:
+        original=e[0]
+        lemma=wnl.lemmatize(original)
+        wordid=e[1]
+        l=LemmaTemp(lemma,original,wordid)
+        db_session.add(l)
+	db_session.commit()
+
 def insertdata():
 	import models
+	
 	from models import Ngrams
 	from models import Phrases
 	allphrases = {}
 	phrase_index= {}
-	questions = Table('questions', metadata, autoload=True)
+	questions = Table('questions', Base.metadata, autoload=True)
 	r = engine.execute('select * from questions_temp')
 	data = r.fetchall()
 	for row in data:
@@ -38,27 +50,15 @@ def insertdata():
 	    for i in range(len(ans)):
 	        for j in range(i+1, len(ans)+1):
 	            phrase = " ".join(ans[i:j])
-	            #print phrase
-	            #phrase = phrase.lower()
-	            
 	            ng=Ngrams(row[0],phrase)
-	            #print ng
 	            db_session.add(ng)
 	            db_session.commit()
-	            #print phrase
-	            #print "----------"
 	            phrase = phrase.lower()
 	            if phrase not in allphrases:
 	                allphrases[phrase] = [phrase.lower()]
 	                phrase_index[phrase] = newPhraseInfo(phrase)
-	            #if phrase =="the blue":
-	                #print "Yes"
-	                #print phrase_index[phrase]
-	                #print "======================"
-	            #phrase_index[phrase] = newPhraseInfo(phrase)
 	            phrase_index[phrase]["count"] += 1
 	            phrase_index[phrase]["ids"].add(str(row[0]))
-	#print phrase_index.keys()
 	i = 0
 	for unique_phrases in phrase_index.keys():
 		l = list(phrase_index[unique_phrases]["ids"])
@@ -69,6 +69,15 @@ def insertdata():
 		print ph
     	db_session.add(ph)
     	db_session.commit() 
-if __name__ == "__main__":
-	insertdata()
+
+if __name__=='__main__':
+    SQLALCHEMY_DATBASE_URI='sqlite:///tutorial.db'
+    engine = create_engine(SQLALCHEMY_DATBASE_URI, convert_unicode=True)
+    db_session = scoped_session(sessionmaker(autocommit=False,
+                                             autoflush=False,
+                                             bind=engine))
+    Base.query = db_session.query_property()
+    init_db()
+    insert_db()
+    insertdata()
 
