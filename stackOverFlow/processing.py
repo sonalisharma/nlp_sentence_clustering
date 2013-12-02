3b7defc4b54c4e52f4b8b268baa160cd018d594c
Sonali <sonali.sharma@ischool.berkeley.edu>
2013-12-01 21:28:33 -0800

    added code to do categorization

--- stackOverFlow/processing.py
+++ stackOverFlow/processing.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from nltk.stem import WordNetLemmatizer as WNL
 from models import Base
 import insert_tables as it
+from collections import Counter
 
 wnl=WNL()
 
@@ -19,27 +20,45 @@ def lemmatize(query):
 def fetchphrases(query): #looks like
 	query_nostopwords = removestopwords(query)
 	query_lemmatized = lemmatize(query_nostopwords) #look like
+	phraseids = []
 	for word in query_lemmatized.split():
+		print "**************"
+		print word
+		print "**************"
 		#query_lemma = "select * from LEMMA_TEMP where lemma='{}'".format(word)
 		#print query_lemma
 		#con = it.engine.execute(query_lemma)
 		#rows = con.fetchall() # 13|look|looks|187   14|look|looking|189
 		#ngram_ids = [ str(data[3]) for data in rows ]
 		#print ngram_ids
-		query_ngram = "select id from ngrams where ngrams.lemmangrams like '%{}%'".format(word)
+		query_ngram = "select id from ngrams where lower(ngrams.lemmangrams) like lower('%{}%')".format(word)
+		print query_ngram
 		con = it.engine.execute(query_ngram)
 		rows_phrase = con.fetchall()
-		print rows_phrase[0]
-		print type(rows_phrase) 
-		ngramids = list(set([i[0] for i in rows_phrase]))
-		categorize(ngramids)
+		if rows_phrase:
+			print "------------"
+			print rows_phrase
+			print "------------"
+			ngramids = list(set([str(i[0]) for i in rows_phrase]))
+		print "@@@@@@@@@@@@@@@@@@@"
+		print ngramids
+		print "@@@@@@@@@@@@@@@@@@@"
+		phraseids.extend(ngramids)
+		phraseids = list(set(phraseids))
+	categorize(phraseids)
 
-def categorize(phraselist):
-	print phraselist
-	print "I am inside categorize"
+def categorize(phraseids):
+	#print phraseids
+	query = "select lemmangrams from ngrams where id in ({})".format(",".join(phraseids))
+	con = it.engine.execute(query)
+	rows_phrase = con.fetchall()
+	#print rows_phrase
+	n = [data[0] for data in rows_phrase]
+	d = Counter(n)
+	return d.most_common(20)
 
 if __name__=='__main__':
-	fetchphrases("looks blue")
+	fetchphrases("gold magi")
 
 
 
