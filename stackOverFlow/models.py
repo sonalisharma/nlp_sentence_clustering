98f2c53af7c9431aee623447da8122a71855d08d
Shubham Goel <shubham.goel@gmail.com>
2013-12-01 17:35:43 -0800

    model modified for answers and questions

--- stackOverFlow/models.py
+++ stackOverFlow/models.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
-from insert_ngrams import Base
-#Base = declarative_base()
+#from insert_clean_so_data import Base
+
+Base = declarative_base()
+
 class Ngrams(Base):
     __tablename__ = 'ngrams'
     __table_args__ = {'extend_existing':True}
@@ -17,7 +19,8 @@ class Ngrams(Base):
         return '<User %s,%s>' %(self.questionid,self.ngrams)
     def returnvalue(self):
         return self.questionid
-    
+
+
 class Phrases(Base):
     __tablename__ = 'phrases'
     __table_args__ = {'extend_existing':True}
@@ -37,16 +40,35 @@ class Phrases(Base):
 class Questions(Base):
     __tablename__ = 'questions'
     __table_args__ = {'extend_existing':True}
-    ques_id = Column(Integer, autoincrement=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
     ques_text = Column(String(3500))
-    answer_text = Column(String(3500))
+    answer_id = Column(Integer)
 
-    def __init__(self, ques_text, answer_text):
+    def __init__(self, ques_id, ques_text, answer_id):
+        self.id = ques_id
         self.ques_text = ques_text
-        self.answer_text = answer_text
+        self.answer_id = answer_id
 
     def __repr__(self):
         return '<Question %s,%s>' % (self.ques_text, self.answer_text)
 
-    def returnvalue(self):
+    def return_value(self):
         return self.ques_text
+
+
+class Answers(Base):
+    __tablename__ = 'answers'
+    __table_args__ = {'extend_existing':True}
+    id = Column(Integer, primary_key=True)
+    answer_text = Column(String(3500))
+    answer_wo_stop_words = Column(String(3500))
+
+    def __init__(self, ans_id, ans_text):
+        self.id = ans_id
+        self.answer_text = ans_text
+
+    def __repr__(self):
+        return '<Answer %s,%s>' % (self.id, self.answer_text)
+
+    def return_value(self):
+        return self.answer_text
