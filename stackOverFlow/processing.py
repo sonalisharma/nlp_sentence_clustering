b996fe4b4e4ecf76b5d2e9a47b34d2944465dcc4
Sonali <sonali.sharma@ischool.berkeley.edu>
2013-12-01 17:44:04 -0800

    Added code for query parsing and categorization

--- stackOverFlow/processing.py
+++ stackOverFlow/processing.py
@@ -4,6 +4,8 @@ import re
 from collections import defaultdict
 from nltk.stem import WordNetLemmatizer as WNL
 from models import Base
+import insert_tables as it
+
 wnl=WNL()
 
 def removestopwords(query):
@@ -14,13 +16,40 @@ def lemmatize(query):
 	wordlist =  [wnl.lemmatize(word) for word in query.split()]
 	return " ".join(wordlist)
 
-def fetchphrases(query):
+def fetchphrases(query): #looks like
 	query_nostopwords = removestopwords(query)
-	query_lemmatized = lemmatize(query_nostopwords)
+	query_lemmatized = lemmatize(query_nostopwords) #look like
 	for word in query_lemmatized.split():
-		db_query = "select * from LemmaTemp where lemma={}".format(word)
-		con = engine.execute(db_query)
-		data = con.fetchall()
+		#query_lemma = "select * from LEMMA_TEMP where lemma='{}'".format(word)
+		#print query_lemma
+		#con = it.engine.execute(query_lemma)
+		#rows = con.fetchall() # 13|look|looks|187   14|look|looking|189
+		#ngram_ids = [ str(data[3]) for data in rows ]
+		#print ngram_ids
+		query_ngram = "select id from ngrams where ngrams.lemmangrams like '%{}%'".format(word)
+		con = it.engine.execute(query_ngram)
+		rows_phrase = con.fetchall()
+		print rows_phrase[0]
+		print type(rows_phrase) 
+		ngramids = list(set([i[0] for i in rows_phrase]))
+		categorize(ngramids)
+
+def categorize(phraselist):
+	print phraselist
+	print "I am inside categorize"
+
+if __name__=='__main__':
+	fetchphrases("looks blue")
+
+
+
+
+
+
+
+
+
+
 
 
 
